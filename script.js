6ae25f8d12460fb3ff7dd3490ac9fd737833ff67
Chris Tolnai <christolnai@gmail.com>
2021-08-22 19:57:11 -0700

    created javascript for showing colors depending on the hour of the day

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var currentDay = document.querySelector("#currentDay");
 var saveBtn = $(".saveBtn");
+var hour = $(".hour");
+var form = document.querySelector(".form");
+var notes = $(".notes");
+
 
 function displayTime() {
     var rightNow = moment().format("dddd, MMMM Do");
@@ -9,11 +13,39 @@ function displayTime() {
 
 
 saveBtn.click(() => {
-    for (let i = 9; i < 17; i++) {
+    
+    for (let i = 9; i < 18; i++) {
       
-        
-        localStorage.setItem(i, $(`#${i}`).val())
+      localStorage.setItem(i, $(`#${i}`).val())
     }
+  
 })
 
 displayTime()
+
+
+var currentHour = moment().format("H");
+
+    for (let i = 9; i < 18; i++) {
+
+        if (currentHour > i)
+        $(`#${i}`).addClass("past");
+
+        else if (currentHour < i)
+        $(`#${i}`).addClass("future");
+
+        else $(`#${i}`).addClass("present");
+    }
+      
+// console.log(currentHour)
+
+
+for (let i = 9; i < 18; i++) {
+      
+    $("#i").append(localStorage.getItem($(`#${i}`)));
+  }
+
+
+
+
+    // $(`#${i}`).textContent=(i, $(`#${i}`));
